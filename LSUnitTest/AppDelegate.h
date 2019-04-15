69c41cccc139225405caf9a2e24cbc02ba2e8775
l694914089 <694914089@qq.com>
2019-04-15 15:20:21 +0800

    单元测试学习项目
    单元测试学习

---
+++ LSUnitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSUnitTest
+//
+//  Created by fei on 2019/4/15.
+//  Copyright © 2019 LSPO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LSUnitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSUnitTest
+//
+//  Created by fei on 2019/4/15.
+//  Copyright © 2019 LSPO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
